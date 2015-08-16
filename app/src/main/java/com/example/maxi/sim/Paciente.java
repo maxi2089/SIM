28826e017ea6a23b459a45ecea07f44e2aa30fca
Sanchez <ae.sanchez89@gmail.com>
2015-08-15 21:19:38 -0300

    Clase nuevas papa

---
+++ app/src/main/java/com/example/maxi/sim/Paciente.java
@@ -0,0 +1,71 @@
+package com.example.maxi.sim;
+
+/**
+ * Created by Sanchez on 15/08/2015.
+ */
+public class Paciente {
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    private String nombre;
+    private String apellido;
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    private String ubicacion;
+    private int ID;
+    private int edad, peso, altura;
+
+}
